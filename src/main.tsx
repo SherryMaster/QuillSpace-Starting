1c5bffa78db5d1cf159b57573266f8c816a89a71
SherryMaster <sherrymaster2@gmail.com>
2025-03-03 12:52:47 +0500

    fix custom scrollbars and a toggle in the main.tsx to show main scrollbar

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Add the class to hide main scrollbar
+document.documentElement.classList.add('hide-main-scrollbar');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
