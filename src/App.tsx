5cd5b4998205a18a66ef0e439004500be20ab0f2
SherryMaster <sherrymaster2@gmail.com>
2025-03-07 04:57:15 +0500

    change timestamps import method

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Greet from './Quill/ReactFundamentals/Greet';
-import { javascript_dsa_timestamps, javascript_timestamps, react_part_1_timestamps, react_part_2_timestamps, react_part_3_timestamps, react_part_4_timestamps, typescript_timestamps } from './Quill/timestamps';
+import * as TimeStamps from './Quill/timestamps';
 import { ContentBlock } from "./components/contentblock/ContentBlock";
 import { ThemedInteractiveContentLayout } from "./components/layout/ThemedInteractiveContentLayout";
 
@@ -9,14 +9,14 @@ function App() {
 
   return (
     <ThemedInteractiveContentLayout
-      title="The Ultimate Interactive 'React' Notes"
+      title="The Ultimate 'Course' Collection"
       titleBG="blue"
-      subtitle="A Modern way of making notes with QuillSpace"
-      emphasizedColors="cyan"
+      subtitle="My curated Courses from Youtube with Awesome Timestamps"
+      emphasizedColors="indigo"
     >
-<ContentBlock
-        title='From JS Noob to React Monster - Full Series'
-        subtitle='Contains 6 courses that will take you from a complete beginner to a React expert.'
+      <ContentBlock
+        title='From JS Noob to JS DSA Master and TypeScript Ninja - Full Series'
+        subtitle='Contains 3 courses that will take you from a complete beginner to a expert TypeScript developer with DSA knowledge.'
         type="Media"
         mediaType='Video'
         timestampsColor='cyan' 
@@ -25,44 +25,47 @@ function App() {
             'https://www.youtube.com/watch?v=H3XIJYEPdus',
             'https://www.youtube.com/watch?v=wBtPGnVnA9g',
             'https://www.youtube.com/watch?v=zeCDuo74uzA',
+          ],
+          titles: [
+            'JavaScript Complete Course',
+            'JavaScript DSA Complete Course',
+            'TypeScript Complete Course',
+          ],
+          timestamps: [
+            TimeStamps.javascript_timestamps,
+            TimeStamps.javascript_dsa_timestamps,
+            TimeStamps.typescript_timestamps,
+          ]          
+        }}
+      />
+
+<ContentBlock
+        title='From JS Noob to React Monster - Full Series'
+        subtitle='Contains 6 courses that will take you from a complete beginner to a expert React developer.'
+        type="Media"
+        mediaType='Video'
+        timestampsColor='cyan' 
+        multiVideo={{
+          urls: [
             'https://www.youtube.com/watch?v=qnwFpjIqsrA',
             'https://www.youtube.com/watch?v=futeaowy34Y',
             'https://www.youtube.com/watch?v=-Qnf2bME-rE',
             'https://www.youtube.com/watch?v=znZQFzoV3CM',
           ],
           titles: [
-            'JavaScript Complete Course',
-            'JavaScript DSA Complete Course',
-            'TypeScript Complete Course',
             'React Mastery (Part 1/4)',
             'React Mastery (Part 2/4)',
             'React Mastery (Part 3/4)',
             'React Mastery (Part 4/4)',
           ],
           timestamps: [
-            javascript_timestamps,
-            javascript_dsa_timestamps,
-            typescript_timestamps,
-            react_part_1_timestamps,
-            react_part_2_timestamps,
-            react_part_3_timestamps,
-            react_part_4_timestamps,
+            TimeStamps.react_part_1_timestamps,
+            TimeStamps.react_part_2_timestamps,
+            TimeStamps.react_part_3_timestamps,
+            TimeStamps.react_part_4_timestamps,
           ]          
         }}
-      /> {/* YouTube video */}
-
-      <ContentBlock
-        title='1: React Fundamentals'
-        type="Classic"
-      >
-        <ContentBlock
-          title='Greet Component'
-          type='Generic'
-        >
-          <Greet />      
-        </ContentBlock>
-      </ContentBlock>
-
+      />
 
     </ThemedInteractiveContentLayout>
   );
